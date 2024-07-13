19815d34e0d08a23d0dadea126f9b4d65ffaa245
Rakibmahmud <hasansaikat74@gmail.com>
2024-07-13 11:42:14 +0600

    added problem 3 and 4

--- problemSolving.js
+++ problemSolving.js
@@ -38,3 +38,42 @@ const getBooksName = (items) => {
 const booksTitle = getBooksName(books);
 
 console.log(booksTitle);
+
+//3.Task: Function Composition
+
+const getSquareNumber = (number) => {
+  return number * number;
+};
+
+const getDoubleNumber = (number) => {
+  return number * 2;
+};
+
+const getAddFive = (number) => {
+  return number + 5;
+};
+
+const getSquareDoubleAddFive = (number) => {
+  return getAddFive(getDoubleNumber(getSquareNumber(number)));
+};
+
+const finalResult = getSquareDoubleAddFive(10);
+console.log(finalResult);
+
+// 4.Task: Sorting Objects
+
+const cars = [
+  { make: "Toyota", model: "Corolla", year: 2020 },
+  { make: "Honda", model: "Civic", year: 2016 },
+  { make: "Ford", model: "Mustang", year: 2027 },
+  { make: "Chevrolet", model: "Camaro", year: 2018 },
+];
+
+const getCarsSortByYear = (items) => {
+  const sortedCar = items.sort((item1, item2) => item1.year - item2.year);
+
+  return sortedCar;
+};
+
+const sortedCar = getCarsSortByYear(cars);
+console.log(sortedCar);
